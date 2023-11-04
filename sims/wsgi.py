369f3cd5784df8fa6995416b3d41c49e121a1d48
Laura <laura.kapitza@gmail.com>
2023-11-04 01:14:01 +0100

    create core domain and index route

--- sims/wsgi.py
+++ sims/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for sims_api project.
+WSGI config for core project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sims_api.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sims.settings')
 
 application = get_wsgi_application()
